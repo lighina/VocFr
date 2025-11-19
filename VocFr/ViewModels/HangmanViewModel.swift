cb56c805ca5679144f64bd4a1c41f5e099c3b078
Junfeng Wang <34605571+lighina@users.noreply.github.com>
2025-11-19 07:17:54 +0100

    delete]

--- VocFr/ViewModels/HangmanViewModel.swift
+++ VocFr/ViewModels/HangmanViewModel.swift
@@ -94,7 +94,7 @@ class HangmanViewModel {
     let availableLetters: [Character] = [
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "à", "â", "é", "è", "ê", "ë", "î", "ï", "ô", "û", "ù", "ü", "ç", "œ", "æ"
+        "à", "â", "é", "è", "ê", "ë", "î", "ï", "ô", "û", "ù", "ü", "ç", "œ"
     ]
 
     /// Remaining incorrect guesses
